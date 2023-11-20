25a39d55bdb4fed59aa928d0a0a3e473d6670675
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-11-20 16:15:58 -0600

    second commit

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+
+function App(){
+    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    let colorMap = colors.map((color, i) => {
+      return (
+        <ColorBlock color={color} />
+      )
+    })
+    return (
+        <div className="App">
+          {colors.map((color, i) => 
+            <ColorBlock key={i} color={color} />
+        )}
+        </div>
+    )
+}
+export default App;
